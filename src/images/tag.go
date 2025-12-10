f458829da3f0da71eabe6da4770ac71cbf799b6b
jeanfrancoisgratton <jean-francois@famillegratton.net>
2025-12-10 08:46:11 -0500

    Added image tag

--- src/images/tag.go
+++ src/images/tag.go
@@ -4,3 +4,34 @@
 // Original filename: src/images/tag.go
 
 package images
+
+import (
+	"dtools2/rest"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	ce "github.com/jeanfrancoisgratton/customError/v3"
+	hftx "github.com/jeanfrancoisgratton/helperFunctions/v4/terminalfx"
+)
+
+// Retag an image
+
+func TagImage(client *rest.Client, oldtag, newtag string) *ce.CustomError {
+	repo, tag := splitURI(newtag)
+	path := "/images/" + oldtag + "/tag?repo=" + repo + "&tag=" + tag
+
+	resp, err := client.Do(rest.Context, http.MethodPost, path, url.Values{}, nil, nil)
+	if err != nil {
+		return &ce.CustomError{Title: "Unable to POST request", Message: err.Error(), Code: 201}
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		return &ce.CustomError{Title: "POST request returned an error", Message: "http requested returned " + resp.Status, Code: 201}
+	}
+	if !rest.QuietOutput {
+		fmt.Println("Image " + hftx.Green(oldtag) + " tagged as " + hftx.Green(newtag))
+	}
+	return nil
+}
