9555270ae2114fc7026b196116fb984f6f216ce6
jeanfrancoisgratton <jean-francois@famillegratton.net>
2026-05-01 07:07:44 -0400

    Fixed the build script's permission denied

--- __archlinux/2.build-package.sh
+++ __archlinux/2.build-package.sh
@@ -4,12 +4,8 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
-rm -rf src pkg
+sudo rm -rf src pkg
 
-PKGDEST=/data makepkg \
-  --force \
-  --clean \
-  --syncdeps \
-  --noconfirm
+PKGDEST=/data makepkg --force --clean --syncdeps --noconfirm
 
-rm -rf src pkg
+sudo rm -rf src pkg
